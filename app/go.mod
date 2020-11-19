4b453b1824a1b10ab75a1c98596bb819b350bfad
Gerardo <ferrari_gerardo@live.com.mx>
2020-11-19 01:06:35 -0600

    Fix jump lines

--- app/go.mod
+++ app/go.mod
@@ -3,6 +3,6 @@ module test
 go 1.13
 
 require (
-	github.com/gerardoCD/log-http-go v0.0.0-20201119062445-13f25869b324
+	github.com/gerardoCD/log-http-go v0.0.0-20201119065912-f7d0bedb3ca7
 	golang.org/x/sys v0.0.0-20201118182958-a01c418693c7 // indirect
 )
--- log_http.go
+++ log_http.go
@@ -20,8 +20,8 @@ func end(){
 }
 
 func Request(json string){
-	color.Blue("-------------------- Request Service ---------------------\n\n\n\n")
-	color.Blue(" Request:  %s \n\n\n\n", json)
+	color.Blue("-------------------- Request Service ---------------------\n\n")
+	color.Blue(" Request:  %s \n\n", json)
 	color.Blue("----------------------------------------------------------\n\n\n\n")
 }
 
@@ -30,17 +30,17 @@ func Reponse(statusCode int , json string){
 
 
 	if (statusCode == 200){
-		color.Green("-------------------- Reponse Service ---------------------\n\n\n\n")
+		color.Green("-------------------- Reponse Service ---------------------\n\n")
 		color.Green(" Status:  %d \n\n", statusCode)
 		color.Green(" Reponse:  %s \n\n", json)
 		color.Green("----------------------------------------------------------\n\n\n\n")
 	} else if ( statusCode >= 201 && statusCode <= 299 ) {
-		color.Magenta("-------------------- Reponse Service ---------------------\n\n\n\n")
+		color.Magenta("-------------------- Reponse Service ---------------------\n\n")
 		color.Magenta(" Status:  %d \n\n", statusCode)
 		color.Magenta(" Reponse:  %s \n\n", json)
 		color.Magenta("----------------------------------------------------------\n\n\n\n")
 	} else {
-		color.Magenta("-------------------- Reponse Service ---------------------\n\n\n\n")
+		color.Magenta("-------------------- Reponse Service ---------------------\n\n")
 		color.Magenta(" Status:  %d \n\n", statusCode)
 		color.Magenta(" Reponse:  %s \n\n", json)
 		color.Magenta("----------------------------------------------------------\n\n\n\n")
@@ -55,21 +55,21 @@ func Reponse(statusCode int , json string){
 func ReponseService(statusCode int ,json string,  url string, name string){
 
 	if (statusCode == 200){
-		color.Green("-------------------- Reponse Service ---------------------\n\n\n\n")
+		color.Green("-------------------- Reponse Service ---------------------\n\n")
 		color.Green(" Name Service:  %s \n\n", name)
 		color.Green(" Url:  %s \n\n", url)
 		color.Green(" Status:  %d \n\n", statusCode)
 		color.Green(" Reponse:  %s \n\n", json)
 		color.Green("----------------------------------------------------------\n\n\n\n")
 	} else if ( statusCode >= 201 && statusCode <= 299 ) {
-		color.Magenta("-------------------- Reponse Service ---------------------\n\n\n\n")
+		color.Magenta("-------------------- Reponse Service ---------------------\n\n")
 		color.Magenta(" Name Service:  %s \n\n", name)
 		color.Magenta(" Url:  %s \n\n", url)
 		color.Magenta(" Status:  %d \n\n", statusCode)
 		color.Magenta(" Reponse:  %s \n\n", json)
 		color.Magenta("----------------------------------------------------------\n\n\n\n")
 	} else {
-		color.Magenta("-------------------- Reponse Service ---------------------\n\n\n\n")
+		color.Magenta("-------------------- Reponse Service ---------------------\n\n")
 		color.Magenta(" Name Service:  %s \n\n", name)
 		color.Magenta(" Url:  %s \n\n", url)
 		color.Magenta(" Status:  %d \n\n", statusCode)
