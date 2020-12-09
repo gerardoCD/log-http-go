a6de69ca98effecd914202bbed4e156e1a372de5
Gerardo <ferrari_gerardo@live.com.mx>
2020-12-08 23:13:20 -0600

    add xml request y respose

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/fatih/color v1.10.0
+	github.com/go-xmlfmt/xmlfmt v0.0.0-20191208150333-d5b6f63a941b
 	github.com/sirupsen/logrus v1.4.2
 	github.com/tidwall/gjson v1.3.5
 )
--- log_http.go
+++ log_http.go
@@ -3,6 +3,9 @@ package log_http
 import (
 	"github.com/fatih/color"
 	"encoding/json"
+	"encoding/xml"
+	"github.com/go-xmlfmt/xmlfmt"
+	"bytes"
 )
 
 
@@ -74,7 +77,20 @@ func RequestService(object interface{},  url string, name string){
 
 
 
+func RequestServiceXML(object interface{},  url string, name string){
+	json,_ := xml.MarshalIndent(object, "", "\t")
+		color.Green("-------------------- Request Service ---------------------\n\n")
+		color.Green(" Name Service:  %s \n\n", name)
+		color.Green(" Url:  %s \n\n", url)
+		color.Green(" Request:  %s \n\n", string(json))
+		color.Green("----------------------------------------------------------\n\n\n\n")
+
+}
+
+
 func RequestServiceString(json string,  url string, name string){
+	// jsonBytes := []byte(json)
+	// jsonPretty,_ := prettyprint(jsonBytes)
 		color.Green("-------------------- Request Service ---------------------\n\n")
 		color.Green(" Name Service:  %s \n\n", name)
 		color.Green(" Url:  %s \n\n", url)
@@ -83,9 +99,47 @@ func RequestServiceString(json string,  url string, name string){
 
 }
 
+func RequestServiceStringXML(xml string,  url string, name string){
+	xmlPretty := xmlfmt.FormatXML(xml, "\t", "  ")
+		color.Green("-------------------- Request Service ---------------------\n\n")
+		color.Green(" Name Service:  %s \n\n", name)
+		color.Green(" Url:  %s \n\n", url)
+		color.Green(" Request:  %s \n\n", xmlPretty)
+		color.Green("----------------------------------------------------------\n\n\n\n")
+
+}
+
 
 func ReponseService(statusCode int ,object interface{},  url string, name string){
 	json,_ := json.MarshalIndent(object, "", "\t")
+	if (statusCode == 200){
+		color.Green("-------------------- Reponse Service ---------------------\n\n")
+		color.Green(" Name Service:  %s \n\n", name)
+		color.Green(" Url:  %s \n\n", url)
+		color.Green(" Status:  %d \n\n", statusCode)
+		color.Green(" Reponse:  %s \n\n", string(json))
+		color.Green("----------------------------------------------------------\n\n\n\n")
+	} else if ( statusCode >= 201 && statusCode <= 299 ) {
+		color.Magenta("-------------------- Reponse Service ---------------------\n\n")
+		color.Magenta(" Name Service:  %s \n\n", name)
+		color.Magenta(" Url:  %s \n\n", url)
+		color.Magenta(" Status:  %d \n\n", statusCode)
+		color.Magenta(" Reponse:  %s \n\n", string(json))
+		color.Magenta("----------------------------------------------------------\n\n\n\n")
+	} else {
+		color.Magenta("-------------------- Reponse Service ---------------------\n\n")
+		color.Magenta(" Name Service:  %s \n\n", name)
+		color.Magenta(" Url:  %s \n\n", url)
+		color.Magenta(" Status:  %d \n\n", statusCode)
+		color.Magenta(" Reponse:  %s \n\n", string(json))
+		color.Magenta("----------------------------------------------------------\n\n\n\n")
+	}
+
+}
+
+
+func ReponseServiceXML(statusCode int ,object interface{},  url string, name string){
+	json,_ := xml.MarshalIndent(object, "", "\t")
 	if (statusCode == 200){
 		color.Green("-------------------- Reponse Service ---------------------\n\n")
 		color.Green(" Name Service:  %s \n\n", name)
@@ -136,6 +190,41 @@ func ReponseServiceString(statusCode int ,json string,  url string, name string)
 		color.Magenta("----------------------------------------------------------\n\n\n\n")
 	}
 
+}
+
+
+func ReponseServiceStringXML(statusCode int ,xml string,  url string, name string){
+	xmlPretty := xmlfmt.FormatXML(xml, "\t", "  ")
+	if (statusCode == 200){
+		color.Green("-------------------- Reponse Service ---------------------\n\n")
+		color.Green(" Name Service:  %s \n\n", name)
+		color.Green(" Url:  %s \n\n", url)
+		color.Green(" Status:  %d \n\n", statusCode)
+		color.Green(" Reponse:  %s \n\n", xmlPretty)
+		color.Green("----------------------------------------------------------\n\n\n\n")
+	} else if ( statusCode >= 201 && statusCode <= 299 ) {
+		color.Magenta("-------------------- Reponse Service ---------------------\n\n")
+		color.Magenta(" Name Service:  %s \n\n", name)
+		color.Magenta(" Url:  %s \n\n", url)
+		color.Magenta(" Status:  %d \n\n", statusCode)
+		color.Magenta(" Reponse:  %s \n\n", xmlPretty)
+		color.Magenta("----------------------------------------------------------\n\n\n\n")
+	} else {
+		color.Magenta("-------------------- Reponse Service ---------------------\n\n")
+		color.Magenta(" Name Service:  %s \n\n", name)
+		color.Magenta(" Url:  %s \n\n", url)
+		color.Magenta(" Status:  %d \n\n", statusCode)
+		color.Magenta(" Reponse:  %s \n\n", xmlPretty)
+		color.Magenta("----------------------------------------------------------\n\n\n\n")
+	}
+
+}
+
+
+func prettyprint(b []byte) ([]byte, error) {
+    var out bytes.Buffer
+    err := json.Indent(&out, b, "", "  ")
+    return out.Bytes(), err
 }
 
 
